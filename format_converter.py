28491f76b5232597ec95f152fa5dbfe44934ac43
Gabri3l-Avi1a <158302206+Gabri3l-Avi1a@users.noreply.github.com>
2024-01-31 16:57:57 +0100

    Update format_converter.py

--- format_converter.py
+++ format_converter.py
@@ -2,19 +2,21 @@ import os
 from PIL import Image
 
 dir = r"C:\Users\gabri\Fish1"
-allowed_formats = ['.png', '.jpeg', '.jpg', '.gif', '.avif']  # List of allowed file formats
+erlaubte_formate = ['.png', '.jpeg', '.jpg', '.gif', '.avif'] 
 
 for filename in os.listdir(dir):
-    if not any(filename.endswith(ext) for ext in allowed_formats):
-        continue  # Skip files with unsupported formats
+    if not any(filename.endswith(ext) for ext in erlaubte_formate):
+        continue  
     name, ext = os.path.splitext(os.path.basename(filename))
     image_path = os.path.join(dir, filename)
-    # Open the image
     Bild = Image.open(image_path)
-    # Convert and save as JPG
-    if ext.lower() == '.gif':  # Convert GIF to JPG
-        Bild.seek(0)  # Go to the first frame
-        Bild = Bild.convert("RGB")  # Convert image to RGB mode (required for JPG)
-    elif ext.lower() == '.avif':  # Convert AVIF to JPG
-        Bild = Bild.convert("RGB")  # Convert image to RGB mode (required for JPG)
+    if ext.lower() == '.gif': 
+        Bild.seek(0)  
+        Bild = Bild.convert("RGB")  
+    elif ext.lower() == '.avif':  
+        Bild = Bild.convert("RGB")  
+    elif ext.lower() == '.jpeg':
+        Bild = Bild.convert('RGB')
+    elif ext.lower() == '.png':
+        Bild = Bild.convert('RGB')
     Bild.save(os.path.join(dir, f"{name}.jpg"))
