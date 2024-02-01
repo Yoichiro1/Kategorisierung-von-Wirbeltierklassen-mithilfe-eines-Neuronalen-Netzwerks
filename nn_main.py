769e43e4dc53750331ce08265f3b627e1259b560
Gabri3l-Avi1a <158302206+Gabri3l-Avi1a@users.noreply.github.com>
2024-02-01 11:38:10 +0100

    Update nn_main.py

--- nn_main.py
+++ nn_main.py
@@ -135,21 +135,3 @@ def apply_Model():
         print("Error, file already exists.")
  model.save(path + "\{filename}.keras")
 apply_Model()
-
-def select_file():
- print('PLACEHOLDER') #PASTE YO'S CODE HIER
-def adjust_file():
-   print('PLACEHOLDER')
-def Manual_Test():
-   print('PLACEHOLDER')
-#Source: https://stackoverflow.com/questions/24072790/how-to-detect-key-presses
-while True:  
-    try:  # used try so that if user pressed other than the given key error will not be shown
-        if kb.is_pressed('x'):
-            select_file()
-            adjust_file()
-            Manual_Test()
-            print("TEST COMPLETED")
-            break  
-    except:
-     break  # if user pressed a key other than the given key the loop will break
