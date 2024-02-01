3e219b207cad90dff165dde8dc534d8efcd89a0d
Gabri3l-Avi1a <158302206+Gabri3l-Avi1a@users.noreply.github.com>
2024-02-01 11:53:29 +0100

    Update nn_load.py

--- Use Network and Load Files/nn_load.py
+++ Use Network and Load Files/nn_load.py
@@ -23,6 +23,26 @@ while image_given = False
         image_given = True
     else:
         print("Error")
+def reformat_image():
+  dir = r"C:\Users\gabri\Fish1"
+  erlaubte_formate = ['.png', '.jpeg', '.jpg', '.gif', '.avif'] 
+
+  for filename in os.listdir(dir):
+    if not any(filename.endswith(ext) for ext in erlaubte_formate):
+        continue  
+    name, ext = os.path.splitext(os.path.basename(filename))
+    image_path = os.path.join(dir, filename)
+    Bild = Image.open(image_path)
+    if ext.lower() == '.gif': 
+        Bild.seek(0)  
+        Bild = Bild.convert("RGB")  
+    elif ext.lower() == '.avif':  
+        Bild = Bild.convert("RGB")  
+    elif ext.lower() == '.jpeg':
+        Bild = Bild.convert('RGB')
+    elif ext.lower() == '.png':
+        Bild = Bild.convert('RGB')
+    Bild.save(os.path.join(dir, f"{name}.jpg"))
 
 img =Image.open(folder_selected)
 img =image.resize((200,200))
