249c5cc15eef60d703fe17defc5adfb5e0dcbf59
Yoichiro1 <158301537+Yoichiro1@users.noreply.github.com>
2024-03-03 17:29:37 +0100

    file_mover.py

--- Image Preparation/file_mover.py
+++ Image Preparation/file_mover.py
@@ -2,28 +2,31 @@ import os
 import shutil
 import random
 
+# absoluten Pfad zum Ordner, worin die Bilder sind, und zum Ziel-Ordner, wohin die Bilder verschoben werden sollen, angeben.
 ordner_pfad = r""
 ziel_ordner_pfad = r""
 
-Auslesen = False
-ziel_ordner_in_anderer_ordner = True
+Auslesen = False # Einstellen, ob nur ein Teil (zufällig ausgewählt) der Bilder verschoben werden soll.
+ordner_in_anderer_ordner = True # Einstellen, ob im oben angegebenen Ordner sich zusätzliche Ordner befinden, worin die Bilder gespeichert sind.
+
 number = 1
 
 def ordener_verschieben(ordner_pfad):
     global ziel_ordner_pfad, Auslesen, number
+    
     for datei in os.listdir(ordner_pfad):
         if Auslesen == True:
-            x = random.randint(1, 101)
+            x = random.randint(1, 100) # Wählt eine zufällige Zahl zwischen 1 und 100 aus.
         else:
             x = 1
-        if x < 51:
+        if x <= 50: # Eine natürliche Zahl angeben. Diese Zahl entspricht dem Prozentsatz, das ein Bild verschoben wird.
             datei_pfad = os.path.join(ordner_pfad, datei)
-            neuer_dateiname = f"common_newt2_{number}.jpg"
+            neuer_dateiname = f"_{number}.jpg" # Einen beliebigen Dateinamen angeben. (Beachten, dass die Dateinamen schon vorhanden sind.)
             ziel_datei_pfad = os.path.join(ziel_ordner_pfad, neuer_dateiname)
-            shutil.move(datei_pfad, ziel_datei_pfad)
+            shutil.move(datei_pfad, ziel_datei_pfad) # Verschiebt die Datei zum Zielordner.
             number += 1
-
-if ziel_ordner_in_anderer_ordner == True:
+# Führt die Funktion oben, je nach Zustand der Variable "ordner_in_anderer_ordner" aus.
+if ordner_in_anderer_ordner == True:
     for u_ordner in os.listdir(ordner_pfad):
         neuer_ordner_pfad = os.path.join(ordner_pfad, u_ordner)
         ordener_verschieben(neuer_ordner_pfad)
