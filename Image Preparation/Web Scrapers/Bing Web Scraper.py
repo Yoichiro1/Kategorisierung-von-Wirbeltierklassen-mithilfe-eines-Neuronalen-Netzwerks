c2825aacb973e7f4dcb04d9a42d9957dfb79ad01
Yoichiro1 <158301537+Yoichiro1@users.noreply.github.com>
2024-03-03 17:10:56 +0100

    Bing Web Scraper.py

--- Image Preparation/Web Scrapers/Bing Web Scraper.py
+++ Image Preparation/Web Scrapers/Bing Web Scraper.py
@@ -1,13 +1,6 @@
-import numpy as np 
-import pandas as pd
-import matplotlib as mp
-import random as rd
-import math as mth
-#These libraries were just to test if importing them with anaconda works
-import bing_image_downloader as bid
-#Importing the bid library
+import bing_image_downloader
 from bing_image_downloader import downloader
-dir= r'C:\Users\gabri\images'
-#Specifically Importing the downloader to not have to type out everything
-downloader.download ("people beige", limit=300, output_dir= dir, adult_filter_off= False, force_replace=False)
-#Actual line of code that does the Web-Scraping
+
+dir= r'' # absoluter Pfad, wo die Bilder gespeichert werden soll.
+
+downloader.download ("", limit=300, output_dir= dir) # Lädt maximal 300 Bilder von "Suchbegriff" im oben festgelegten Ordner herunter.
