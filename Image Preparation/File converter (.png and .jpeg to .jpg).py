8e6a79c2469a6e79d84db3580b75bb2c11d40040
Yoichiro1 <158301537+Yoichiro1@users.noreply.github.com>
2024-03-03 17:19:45 +0100

    File converter (to .jpg).py

--- Image Preparation/File converter (.png and .jpeg to .jpg).py
+++ Image Preparation/File converter (.png and .jpeg to .jpg).py
@@ -1,17 +1,23 @@
 import os
 from PIL import Image
 
-dir = r"C:\Users\gabri\images\allimages"
+dir = r"" # absoluter Pfad zum Ordner, worin die Bilder gespeichert sind.
+
 erlaubte_formate = ['.png', '.jpeg', '.jpg', '.gif', '.avif', 'img', 'webp', 'tiff'] 
 
 for filename in os.listdir(dir):
+    # Überprüft, ob das Bild ein erlaubtes Format hat
     if not any(filename.endswith(ext) for ext in erlaubte_formate):
+        os.remove(os.path.join(dir, os.path.basename(filename)))
         continue  
+        
     name, ext = os.path.splitext(os.path.basename(filename))
     image_path = os.path.join(dir, filename)
     Bild = Image.open(image_path)
+    
+    # konvertiert das Bild is RGB-Format, falls das Bild nicht eine ".jpg"-Datei ist, damit das Neuronale Netzwerk alle Bilder verwenden kann
     if ext.lower() == '.gif': 
-        Bild.seek(0)  
+        Bild.seek(0)  # Das Bild wird zum ersten Frame zurückgespult 
         Bild = Bild.convert("RGB")  
     elif ext.lower() == '.avif':  
         Bild = Bild.convert("RGB")  
@@ -27,5 +33,6 @@ for filename in os.listdir(dir):
         Bild = Bild.convert('RGB')
     elif ext.lower() == ".jpg":
         continue
-    Bild.save(os.path.join(dir, f"{name}.jpg"))
-    os.remove(os.path.join(dir, os.path.basename(filename)))
+        
+    Bild.save(os.path.join(dir, f"{name}.jpg")) # Das Bild wird als eine ".jpg"-Datei gespreichert
+    os.remove(os.path.join(dir, os.path.basename(filename))) # Das ursprüngliche Bild wird gelöscht
