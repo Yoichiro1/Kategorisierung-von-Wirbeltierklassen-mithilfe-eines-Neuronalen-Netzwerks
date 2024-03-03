85bac783019a69c28e4385919cf2b7ad5c02dc3e
Yoichiro1 <158301537+Yoichiro1@users.noreply.github.com>
2024-03-03 17:31:58 +0100

    Update file_renamer.py

--- Image Preparation/file_renamer.py
+++ Image Preparation/file_renamer.py
@@ -1,10 +1,10 @@
 import os
-os.chdir('Bird Photos')
-print(os.getcwd())
- 
-for count, f in enumerate(os.listdir()):
+
+dir = r"" # absoluter Pfad zum Ordner, worin die Bilder gespeichert sind.
+
+for count, f in enumerate(os.listdir(dir)):
     f_name, f_ext = os.path.splitext(f)
-    f_name = "vogel_" + str(count+1)
+    f_name = "_" + str(count+1) # einen beli
  
     new_name = f'{f_name}{f_ext}'
     os.rename(f, new_name)
