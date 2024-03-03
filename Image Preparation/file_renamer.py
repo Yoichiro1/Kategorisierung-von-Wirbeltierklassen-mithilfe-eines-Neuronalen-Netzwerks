0a80e0dfa5cc9b3e9082c3b558a4106009be6941
Yoichiro1 <158301537+Yoichiro1@users.noreply.github.com>
2024-03-03 17:33:11 +0100

    file_renamer.py

--- Image Preparation/file_renamer.py
+++ Image Preparation/file_renamer.py
@@ -4,7 +4,6 @@ dir = r"" # absoluter Pfad zum Ordner, worin die Bilder gespeichert sind.
 
 for count, f in enumerate(os.listdir(dir)):
     f_name, f_ext = os.path.splitext(f)
-    f_name = "_" + str(count+1) # einen beli
- 
+    f_name = "_" + str(count+1) # Einen beliebigen Dateinamen angeben. (Beachten, dass die Dateinamen schon vorhanden sind.)
     new_name = f'{f_name}{f_ext}'
-    os.rename(f, new_name)
+    os.rename(f, new_name) # Benennt die Datei um.
