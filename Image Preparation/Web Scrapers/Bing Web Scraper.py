c715d2660090b08097d24e7e89260a58dc595002
Yoichiro1 <158301537+Yoichiro1@users.noreply.github.com>
2024-04-22 20:06:56 +0200

    Delete Image Preparation/Web Scrapers directory

--- Image Preparation/Web Scrapers/Bing Web Scraper.py
+++
@@ -1,6 +0,0 @@
-import bing_image_downloader
-from bing_image_downloader import downloader
-
-dir= r'' # absoluter Pfad, wo die Bilder gespeichert werden soll.
-
-downloader.download ("", limit=300, output_dir= dir) # Lädt maximal 300 Bilder von "Suchbegriff" im oben festgelegten Ordner herunter.
