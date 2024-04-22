1d798ccfa20d4aa8bd1d4a31993bf7ffa79f327e
Yoichiro1 <158301537+Yoichiro1@users.noreply.github.com>
2024-04-22 20:20:06 +0200

    nn_main.py

--- Create Neural Network/nn_main.py
+++ Create Neural Network/nn_main.py
@@ -1,8 +1,6 @@
 # Alle Module importieren
 import numpy as np
 import cv2
-import random as rd
-import math as m
 import keras
 import tensorflow as tf
 from keras.models import Sequential as sq
@@ -10,11 +8,10 @@ from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from keras import layers 
 from keras import models
 import os
-import pandas
 import matplotlib.pyplot as plt
 
 
-# Den absoluten Pfad des Ordners für Trainings- und Validierungsbilder einsetzen
+# Den absoluten Pfad des Ordners für Trainings- und Testbilder einsetzen
 train_dir = r"C:\Users\aokik\pyworks\ds_train_images_unaugmentated\ds_train_images"
 test_dir = r"C:\Users\aokik\pyworks\allimages"
 
@@ -22,18 +19,18 @@ test_dir = r"C:\Users\aokik\pyworks\allimages"
 num_epochs = 20
 
 
-#Bilder laden
+# Bilder laden
 train_ds = keras.utils.image_dataset_from_directory(
-    directory=train_dir, #Pfad zum Ordner
-    labels='inferred', #Definiert Label der Daten nach der Struktur des Ordners
-    label_mode='categorical', #Definiert den Modus der Label: kategorisch
-    batch_size=20, #Definiert die Grösse eines Batches
-    image_size=(50, 50)) #Grösse der vom Netzwerk verarbeiteten Bilder 
+    directory=train_dir, # Pfad zum Ordner
+    labels='inferred', # Definiert Label der Daten nach der Struktur des Ordners
+    label_mode='categorical', # Definiert den Modus der Label: kategorisch
+    batch_size=200, # Definiert die Grösse eines Batches
+    image_size=(50, 50)) # Grösse der vom Netzwerk verarbeiteten Bilder 
 validation_ds = keras.utils.image_dataset_from_directory(
     directory=test_dir,
     labels='inferred',
     label_mode='categorical',
-    batch_size=10,
+    batch_size=50,
     image_size=(50, 50))
 
 
@@ -43,7 +40,7 @@ def preprocess_image(image, label):
     image = image / 255.0 # Dividiert die Pixelwerte durch 255, damit sie im Bereich von 0-1 sind. Dient der Stabiliät des Netzwerks.
     return image, label
 
-# Anwendung der Funktion auf die Trainings- und Validierungsbilder
+# Anwendung der Funktion auf die Trainings- und Testbilder
 train_ds = train_ds.map(preprocess_image)
 validation_ds = validation_ds.map(preprocess_image)
 
@@ -51,7 +48,7 @@ validation_ds = validation_ds.map(preprocess_image)
 # Das Neuronale Netzwerk aufbauen
 def apply_Model():
     model = keras.Sequential([ # Modell definieren durch "Sequential"(gruppiert mehrere Schichten)    
-        layers.Conv2D(32, kernel_size=(3, 3), activation="relu", input_shape = (50, 50, 3)), # Eine Convolutional-Schicht mit 32 3x3 Filter, mit der Aktivierungsfunktion "ReLu" (f(x)=max(0,x)) und mit einer Eingabeform des Bildes von 50x50x3 (RGB).
+        layers.Conv2D(32, kernel_size=(3, 3), activation="relu", input_shape = (50, 50, 3)), # Eine Convolutional-Schicht mit 32 3x3 Filter, mit der Aktivierungsfunktion "ReLu" und mit einer Eingabeform des Bildes von 50x50x3 (RGB).
         layers.BatchNormalization(), # Eine BatchNormalization-Schicht, die die Aktivierungswerte eines neuronalen Netzwerks normalisieren.
         layers.MaxPooling2D(pool_size=(2, 2)), # Eine MaxPooling-Schicht, die nur den maximalen Wert für jedes 2x2 Feld im Bild weitergibt.
         layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
@@ -73,12 +70,12 @@ def apply_Model():
     ])
     # Modell für das Training konfigurieren
     global history
-    history = model.compile(optimizer= keras.optimizers.Adam(learning_rate = 0.001), # Definiert den Optimierer des Modells als "adam" (eine stochastische Gradientenabstiegsmethode, die auf der adaptiven Schätzung von Momenten erster und zweiter Ordnung basiert) mit Learning-Rate von 0.0005
-                  loss='categorical_crossentropy', # Definiert Loss-Funktion als "categorical_crossentropy": 
+    history = model.compile(optimizer= keras.optimizers.Adam(learning_rate = 0.001), # Definiert den Optimierer des Modells als "adam" (eine stochastische Gradientenabstiegsmethode, die auf der adaptiven Schätzung von Momenten erster und zweiter Ordnung basiert) mit Learning-Rate von 0.001
+                  loss='categorical_crossentropy', # Definiert Loss-Funktion als "categorical_crossentropy": Gibt an, wie weit die Vorhersage des Modells von den richtigen Werten entfernt sind.
                   metrics=['accuracy']) # Definiert die Metrik (beurteil das Modell) als "accuracy" (gibt die Genauigkeit in Dezimaldarstellung an)
     
     # Modell trainieren
-    model.fit(train_ds, epochs=num_epochs, validation_data = validation_ds) # Trainingsverfahren des Modells, nach jeder Epoche wird das Modell mit den Validierungsdaten getestet
+    model.fit(train_ds, epochs=num_epochs, validation_data = validation_ds) # Trainingsverfahren des Modells, nach jeder Epoche wird das Modell mit den Testdaten getestet
 
     # Modell speichern
     filename_given = False
