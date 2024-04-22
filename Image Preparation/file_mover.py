1e03237a1067ecdd861e9bcd16eade205601cb48
Yoichiro1 <158301537+Yoichiro1@users.noreply.github.com>
2024-04-22 20:24:12 +0200

    file_mover.py

--- Image Preparation/file_mover.py
+++ Image Preparation/file_mover.py
@@ -21,11 +21,11 @@ def ordener_verschieben(ordner_pfad):
             x = 1
         if x <= 50: # Eine natürliche Zahl angeben. Diese Zahl entspricht dem Prozentsatz, dass ein Bild verschoben wird.
             datei_pfad = os.path.join(ordner_pfad, datei)
-            neuer_dateiname = f"_{number}.jpg" # Einen beliebigen Dateinamen angeben. (Beachten, dass die Dateinamen schon vorhanden sind.)
+            neuer_dateiname = f"_{number}.jpg" # Einen beliebigen Dateinamen angeben. (Beachten, dass die Dateinamen nicht schon vorhanden sind.)
             ziel_datei_pfad = os.path.join(ziel_ordner_pfad, neuer_dateiname)
             shutil.move(datei_pfad, ziel_datei_pfad) # Verschiebt die Datei zum Zielordner.
             number += 1
-# Führt die Funktion oben, je nach Zustand der Variable "ordner_in_anderer_ordner" aus.
+# Führt die Funktion oben je nach Zustand der Variable "ordner_in_anderer_ordner" aus.
 if ordner_in_anderer_ordner == True:
     for u_ordner in os.listdir(ordner_pfad):
         neuer_ordner_pfad = os.path.join(ordner_pfad, u_ordner)
